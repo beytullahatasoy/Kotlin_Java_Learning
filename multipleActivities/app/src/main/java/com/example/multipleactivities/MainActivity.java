8bffa19afc1b3e5995467da83957d2c0a8ae6d92
beytullahatasoy <beytullahatasoy55@gmail.com>
2025-07-24 14:44:38 +0300

    Initial commit

--- multipleActivities/app/src/main/java/com/example/multipleactivities/MainActivity.java
+++ multipleActivities/app/src/main/java/com/example/multipleactivities/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.multipleactivities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +13,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    String userName;
+    EditText editText;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editTextText);
+
+        userName = "";
     }
 
     public void changeActivity (View view){
+
+        userName = editText.getText().toString();
+
         Intent intent = new Intent(this, MainActivity2.class);
+
+        intent.putExtra("userInput", userName);
+
         startActivity(intent);
+
+
+
     }
 
 
--- multipleActivities/app/src/main/java/com/example/multipleactivities/MainActivity2.java
+++ multipleActivities/app/src/main/java/com/example/multipleactivities/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.multipleactivities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +11,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
 public class MainActivity2 extends AppCompatActivity {
+
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        textView = findViewById(R.id.textView2);
+
+        Intent intent = getIntent();
+        String userName = intent.getStringExtra("userInput");
+        textView.setText(userName);
+
     }
 
     public void changeScreen (View view) {
